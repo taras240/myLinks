e3fa60d90ef5a820a81bf031fbe5b361d57c9a1a
taras240 <khomyn@outlook.com>
2024-02-03 22:02:02 +0200

    add folders

--- js/main.js
+++ js/main.js
@@ -22,8 +22,16 @@ const MONTH_OF_A_YEAR = {
   12: "Грудня",
 };
 
-const container = document.querySelector(".links-container");
+//////////////////////////////////////////
+//////   ELEMENTS INITIALIZATION     /////
+//////////////////////////////////////////
+
+let SHOW_HIDDEN = localStorage.getItem("SHOW_HIDDEN") === "true";
+const linksContainer = document.querySelector(".links-container");
 const headerClock = document.querySelector("#header-clock");
+const folderContainer = document.querySelector(".folder_container");
+const folderLinkContainer = document.querySelector(".folder-links");
+var myLinks;
 
 function formatNumber(num, size = 2) {
   return num.toLocaleString("en-US", {
@@ -32,23 +40,120 @@ function formatNumber(num, size = 2) {
   });
 }
 
-var createLinkElements = (linksArray) => {
-  linksArray.forEach((link) => {
-    let linkElement = document.createElement("div");
-    linkElement.className = "link-card";
-    linkElement.innerHTML = `
-          <a class="link-main-information" href="${link.links[0]}">
-          <img class="link-preview" src="./src/img/${
-            link.preview ? link.preview : "link.png"
-          }" alt=""></img>
-          <div class="link-name">${link.name}</div>
-         
-      </a>
-      `;
-    container.appendChild(linkElement);
+//////////////////////////////////////////
+//////   READ LINKS FROM JSON        /////
+//////////////////////////////////////////
+
+var readLinksFromJSON = ({ jsonName, container }) => {
+  fetch(`../src/${jsonName}.json`)
+    .then((resp) => resp.json())
+    .then((links) => {
+      links.forEach((link) => {
+        let element = parseElement({ link: link });
+        element ? container.appendChild(element) : "";
+      });
+    });
+};
+var parseElement = ({ showHidden = SHOW_HIDDEN, link }) => {
+  switch (link.type) {
+    case "folder":
+      return makeFolderElement(link);
+      break;
+    default:
+      return makeLinkElement(link);
+      break;
+  }
+};
+var makeLinkElement = (link) => {
+  if (!SHOW_HIDDEN && link.hidden === true) return;
+  let linkElement = document.createElement("div");
+  linkElement.className = "link-card";
+  linkElement.innerHTML = `
+        <a class="link-main-information" href="${link.links[0]}">
+        <img class="link-preview" src="./src/img/${
+          link.preview ? link.preview : "link.png"
+        }" alt=""></img>
+        <div class="link-name">${link.name}</div>
+       
+    </a>
+    `;
+  return linkElement;
+};
+var makeFolderElement = (folder) => {
+  let folderElement = document.createElement("div");
+  folderElement.className = "folder-card";
+  if (!SHOW_HIDDEN && folder.hidden === true)
+    folderElement.classList.add("hidden");
+  folderElement.setAttribute("data-private", folder.hidden);
+  folderElement.innerHTML = `
+        <a class="link-main-information">
+        <img class="link-preview" src="./src/img/${
+          folder.preview ? folder.preview : "folde.png"
+        }" alt=""></img>
+        <div class="link-name">${folder.name}</div>
+       
+    </a>
+    `;
+
+  folderElement.addEventListener("click", () => {
+    openFoler(folder);
   });
+  return folderElement;
 };
 
+readLinksFromJSON({ jsonName: "links-list", container: linksContainer });
+
+//////////////////////////////////////////////
+//////     ADD EVENTS LISTINERS     //////////
+//////////////////////////////////////////////
+
+/// PRESS SOME BUTTON EVENTS       ///////////
+document.addEventListener("keydown", (e) => {
+  switch (e.code) {
+    case "KeyX":
+      SHOW_HIDDEN = !SHOW_HIDDEN;
+      localStorage.setItem("SHOW_HIDDEN", SHOW_HIDDEN);
+      let hiddenElements = document.querySelectorAll("[data-private = 'true']");
+      hiddenElements.forEach((el) => {
+        SHOW_HIDDEN
+          ? el.classList.remove("hidden")
+          : el.classList.add("hidden");
+      });
+      break;
+    default:
+      console.log(e.code);
+      break;
+  }
+});
+
+////  CLOSE FOLDER BUTTON    ///////////////
+document.querySelector(".close-folder_button").addEventListener("click", () => {
+  closeFolder();
+});
+
+////////////////////////////////////////////
+/////   ADDITIONAL FUNCTIONS     ///////////
+////////////////////////////////////////////
+
+////   OPEN / CLOSE FOLDER    //////////////////////
+var openFoler = (folder) => {
+  if (!folderContainer.classList.contains("active")) {
+    document.querySelector(".folder-header").innerText = folder.name;
+    folderContainer.classList.add("active");
+    readLinksFromJSON({
+      jsonName: folder.listName,
+      container: folderLinkContainer,
+    });
+  } else {
+    closeFolder();
+  }
+};
+var closeFolder = () => {
+  folderContainer.classList.remove("active");
+  folderLinkContainer.innerHTML = "";
+};
+
+//////   UPDATE TIME  /////////////////////
 var updateTime = () => {
   var time = new Date();
   var curTime = `${formatNumber(time.getHours())}:${formatNumber(
@@ -59,10 +164,5 @@ var updateTime = () => {
   }`;
   headerClock.innerText = curDate + "\t" + curTime;
 };
-
-fetch("./src/links-list.json")
-  .then((resp) => resp.json())
-  .then((links) => createLinkElements(links));
-
 updateTime();
 const timeInterval = setInterval(updateTime, 1000);
