86d687367ae04d886efdfe9d09137197d0beef3b
taras240 <khomyn@outlook.com>
2024-02-06 15:11:39 +0200

    fix bug

--- js/docElements.js
+++ js/docElements.js
@@ -11,14 +11,3 @@ const creationContainer = document.querySelector(".creator_container");
 ////        WEATHER ELEMENTS
 
 const weatherContainer = document.querySelector(".weather_container");
-const weatherCards = [];
-document.querySelectorAll(".weather-card").forEach((card) => {
-  let dayCard = {
-    day: card.querySelector(".weather-card_day"),
-    minTemp: card.querySelector(".weather-card_min-temp"),
-    maxTemp: card.querySelector(".weather-card_max-temp"),
-    icon: card.querySelector(".weather-card_max-icon"),
-    precipitation: card.querySelector(".weather-card_precipitation-prob"),
-  };
-  weatherCards.push(dayCard);
-});
